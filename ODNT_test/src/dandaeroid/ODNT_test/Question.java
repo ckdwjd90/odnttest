9cd569fcd054fa50c3f4626b39841276bbc33945
templars <templars@c68f62c1-04b1-7cd3-8464-b6102c149504>
2010-08-11 08:21:17 +0000

    Question.java 업데이트

--- ODNT_test/src/dandaeroid/ODNT_test/Question.java
+++ ODNT_test/src/dandaeroid/ODNT_test/Question.java
@@ -6,8 +6,15 @@ public class Question {
 	int _cate02;
 	int _answer;
 	String _solution;
-	int rate;
+	int _rate;
 
-	public Question() {
+	public Question(String _keyword, int _cate01, int _cate02, int _answer,
+			String _solution, int _rate) {
+		this._keyword = _keyword;
+		this._cate01 = _cate01;
+		this._cate02 = _cate02;
+		this._answer = _answer;
+		this._solution = _solution;
+		this._rate = _rate;
 	}
 }
