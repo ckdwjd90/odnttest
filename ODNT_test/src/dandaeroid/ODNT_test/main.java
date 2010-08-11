815f47d34f35e236729d9ae12364675b62569c0c
templars <templars@c68f62c1-04b1-7cd3-8464-b6102c149504>
2010-08-11 08:16:59 +0000

    main 액티비티 구현~

--- ODNT_test/src/dandaeroid/ODNT_test/main.java
+++ ODNT_test/src/dandaeroid/ODNT_test/main.java
@@ -1,13 +1,39 @@
 package dandaeroid.ODNT_test;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class main extends Activity {
-    /** Called when the activity is first created. */
+	Button _btQuestion_reg;
+	Button _btView_question;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        _btQuestion_reg = (Button)findViewById(R.id.Button01);
+        _btView_question = (Button)findViewById(R.id.Button02);
+        
+        _btQuestion_reg.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(main.this, question_reg.class);
+				startActivity(intent);
+			}
+        });
+        
+        _btView_question.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(main.this, view_question.class);
+				startActivity(intent);
+			}
+        });
+
     }
 }
