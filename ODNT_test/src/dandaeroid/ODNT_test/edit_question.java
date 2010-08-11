dffb05129e4afd7d68105bd886c1939e46b74a67
templars <templars@c68f62c1-04b1-7cd3-8464-b6102c149504>
2010-08-11 09:28:28 +0000

    edit_question.Act 만드는 중

--- ODNT_test/src/dandaeroid/ODNT_test/edit_question.java
+++ ODNT_test/src/dandaeroid/ODNT_test/edit_question.java
@@ -1,15 +1,36 @@
 package dandaeroid.ODNT_test;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
 public class edit_question extends Activity {
+	int _position;
+	Global _global;
+	Question _question;
+	
+	EditText _etKeyword;
+	ArrayList<String> _alCate01;
+	ArrayList<String> _alCate02;
+	ArrayList<String> _alAnswer;
+	EditText _etSolution;
+	ArrayList<String> _alRate;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-	
+        
+        Intent intent = getIntent();
+        _position = intent.getIntExtra("position", 65535);
+        _question = _global._alQuestion.get(_position);
+        
+        _etKeyword.setText(_question._keyword);
+        _etSolution.setText(_question._solution);
+        
 	}
-
 }
