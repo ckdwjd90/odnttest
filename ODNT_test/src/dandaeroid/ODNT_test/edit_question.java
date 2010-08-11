e4d8caa163378e0a93cfe31db728c737855086d6
templars <templars@c68f62c1-04b1-7cd3-8464-b6102c149504>
2010-08-11 08:40:34 +0000

    edit_question.Act 추가

---
+++ ODNT_test/src/dandaeroid/ODNT_test/edit_question.java
@@ -0,0 +1,15 @@
+package dandaeroid.ODNT_test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class edit_question extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+	
+	}
+
+}
