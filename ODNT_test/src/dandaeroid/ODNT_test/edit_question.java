f42578324046beadaf5a97880320189b4b285dca
templars <templars@c68f62c1-04b1-7cd3-8464-b6102c149504>
2010-08-12 07:22:23 +0000

    edit_quiestion.Act 진행중~

--- ODNT_test/src/dandaeroid/ODNT_test/edit_question.java
+++ ODNT_test/src/dandaeroid/ODNT_test/edit_question.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class edit_question extends Activity {
 	int _position;
@@ -13,11 +15,17 @@ public class edit_question extends Activity {
 	Question _question;
 	
 	EditText _etKeyword;
-	ArrayList<String> _alCate01;
-	ArrayList<String> _alCate02;
+	Spinner _spCate01;
+	ArrayAdapter<String> _aaCate01;
+	Spinner _spCate02;
+	ArrayAdapter<String> _aaCate02;
+	Spinner _spAnswer;
 	ArrayList<String> _alAnswer;
+	ArrayAdapter<String> _aaAnswer;
 	EditText _etSolution;
+	Spinner _spRate;
 	ArrayList<String> _alRate;
+	ArrayAdapter<String> _aaRate;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,11 @@ public class edit_question extends Activity {
         setContentView(R.layout.edit_question);
         
         _global = (Global)getApplication();
+        _etKeyword = (EditText)findViewById(R.id.EditText01);
+        _spCate01 = (Spinner)findViewById(R.id.Spinner01);
+        _spCate02 = (Spinner)findViewById(R.id.Spinner02);
+        _spAnswer = (Spinner)findViewById(R.id.Spinner03);
+        _etSolution = (EditText)findViewById(R.id.EditText02);
         
         Intent intent = getIntent();
         _position = intent.getIntExtra("position", 0);
