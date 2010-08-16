bbb09964d8ddeb0cccb324c66c2ffa82bfd4df33
templars <templars@c68f62c1-04b1-7cd3-8464-b6102c149504>
2010-08-16 06:31:42 +0000

    전역 객체 갱신

--- ODNT_test/src/dandaeroid/ODNT_test/Global.java
+++ ODNT_test/src/dandaeroid/ODNT_test/Global.java
@@ -7,7 +7,11 @@ import android.app.Application;
 public class Global extends Application {
 	ArrayList<Question> _alQuestion;
 	ArrayList<String> _alCate01;
+	ArrayList<String> _alCate01Edited;
+	ArrayList<Integer> _alCate01Position;
 	ArrayList<String> _alCate02;
+	ArrayList<String> _alCate02Edited;
+	ArrayList<Integer> _alCate02Position;
 
 	public void onCreate(){
 		_alQuestion = new ArrayList<Question>();
@@ -15,6 +19,10 @@ public class Global extends Application {
 		_alCate02 = new ArrayList<String>();
 		
         _alCate01.add("미분류");
+        _alCate01Edited.add("미분류");
+        _alCate01Position.add(0);
         _alCate02.add("미분류");
+        _alCate01Edited.add("미분류");
+        _alCate02Position.add(0);
 	}
 }
