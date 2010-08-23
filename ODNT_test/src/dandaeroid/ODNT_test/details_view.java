f82086eb5a4de65fb3ed1027283f5d586687a060
sangtaik <sangtaik@c68f62c1-04b1-7cd3-8464-b6102c149504>
2010-08-23 08:28:17 +0000

    before&next 기능 추가 xml ->TextView02 추가

--- ODNT_test/src/dandaeroid/ODNT_test/details_view.java
+++ ODNT_test/src/dandaeroid/ODNT_test/details_view.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class details_view extends Activity {
@@ -13,9 +13,13 @@ public class details_view extends Activity {
 	Global _global;
 	Question q;
 	TextView _text;
-	Button _bt_ok;
-	Button _bt_edit_question;
-	String st;
+	TextView _m_text;
+	ImageButton _bt_before;
+	ImageButton _bt_after;
+	ImageButton _bt_ok;
+	ImageButton _bt_edit_question;
+	String _st;
+	String _message;
 	int position;
 
 	/** Called when the activity is first created. */
@@ -26,15 +30,64 @@ public class details_view extends Activity {
 		_intent = getIntent();
 		_global = (Global) getApplication();
 		_text = (TextView) findViewById(R.id.TextView01);
-		_bt_ok = (Button) findViewById(R.id.Button01);
-		_bt_edit_question = (Button) findViewById(R.id.Button02);
+		_m_text = (TextView) findViewById(R.id.TextView02);
+		_bt_before = (ImageButton) findViewById(R.id.ImageButton01);
+		_bt_after = (ImageButton) findViewById(R.id.ImageButton02);
+		_bt_ok = (ImageButton) findViewById(R.id.ImageButton03);
+		_bt_edit_question = (ImageButton) findViewById(R.id.ImageButton04);
 		position = _intent.getIntExtra("position", 0);
 
-		q = _global._alQuestion.get(position);
-		st = "keyword: " + q._keyword + "\n"+ " q._cate01: "  + q._cate01 + "\n" + "q._cate02: " + q._cate02 + "\n" + " q._answer: "  + q._answer
-				+ "\n" + " q._solution: " + q._solution + "\n" + " q._rate: " + q._rate + "\n";
-		_text.setText(st.toString());
-		// _text.setText(st.toString());
+		if (_global._alQuestion.isEmpty()) {
+			_st = "";
+			_message = "It is Empty ";
+			_text.setText(_message.toString());
+		} else {
+			q = _global._alQuestion.get(position);
+			_Print_Question();
+			_text.setText(_st.toString());
+
+			_bt_before.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+
+					if (position > 0) {
+						position = position - 1;
+						
+						q = _global._alQuestion.get(position);
+						_message = "";
+						_Print_Question();
+						_text.setText(_st.toString());
+					} else {
+						_message = "It is first page & size is "
+						+ _global._alQuestion.size() + "\n";
+						_Print_Question();
+						_text.setText(_st.toString());
+						_m_text.setText(_message.toString());
+					}
+				}
+			});
+
+			_bt_after.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					if (position < (_global._alQuestion.size() - 1)) {
+						position = position + 1;
+						
+						q = _global._alQuestion.get(position);
+						_message = "";
+						_Print_Question();
+						_text.setText(_st.toString());
+						_m_text.setText(_message.toString());
+					} else {
+						_message = "It is final page & size is "
+								+ _global._alQuestion.size() + "\n";
+						_Print_Question();
+						_text.setText(_st.toString());
+						_m_text.setText(_message.toString());
+					}
+				}
+			});
+		}
 
 		_bt_ok.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
@@ -54,4 +107,16 @@ public class details_view extends Activity {
 		// TODO Auto-generated method stub
 	}
 
+	public void  _Print_Question() {
+		_st ="Page: " + position + "\n" + 
+			"keyword: " + q._keyword + "\n" + 
+			" q._cate01: " + q._cate01+ "\n" + 
+			"q._cate02: " + q._cate02 + "\n" + 
+			" q._answer: "	+ q._answer + "\n" + 
+			" q._rate: " + q._rate + "\n" + 
+			" q._solution: " + q._solution + "\n";
+		
+	}
 }
+
+
